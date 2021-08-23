c167d42a130e1b355450fd8485fdaafa8db6c5f7
Dan Ottey <dottey@boomi.com>
2021-08-23 08:22:50 -0400

    Can now get md5sums from entire directory

--- dupefinder.py
+++ dupefinder.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 def get_md5(filename):
     # https://stackoverflow.com/questions/16874598/how-do-i-calculate-the-md5-checksum-of-a-file-in-python/16876405
@@ -12,4 +13,16 @@ def get_md5(filename):
 
     return file_hash.hexdigest()
 
-print(get_md5('data/20210713-amfilm_cover_crack1.jpeg'))
+def get_md5_dir(dir):
+    md5sums = []
+    for root, _, files in os.walk(dir, topdown = False):
+        for name in files:
+            file = os.path.join(root, name)
+            md5sums.append((get_md5(file), file))
+
+    return md5sums
+
+# print(get_md5('data/20210713-amfilm_cover_crack1.jpeg'))
+# print()
+for md5sum_pair in get_md5_dir('data'):
+    print(md5sum_pair[0], md5sum_pair[1])
