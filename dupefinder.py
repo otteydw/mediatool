6c9e610367a215e007548a12066d10747a9ad89f
Daniel W. Ottey <dan.ottey@boomi.com>
2024-05-23 09:15:52 -0400

    Prompt for which dupe to keep. But don't do anything with that data yet.

--- dupefinder.py
+++ dupefinder.py
@@ -1,22 +1,26 @@
 import logging
 from pathlib import Path
 
+from more_itertools import first
 from rich.console import Console
+from rich.prompt import IntPrompt
 from rich.table import Table
 from sqlalchemy import create_engine, select, text
 from sqlalchemy.orm import Session
 
 from logging_setup import logging_setup
 from models import Base, File
-from utils import load_config, recommended_filename
+from utils import get_recommended_filename, load_config
 
 logging_setup()
 logger = logging.getLogger(__name__)
 
 
-debug = True
+debug = False
 if debug:
     logger.setLevel(logging.DEBUG)
+# else:
+#     logger.setLevel(logging.WARN)
 
 
 def find_duplicate_checksums(session: Session):
@@ -26,29 +30,45 @@ def find_duplicate_checksums(session: Session):
 
 
 def process_duplicates(session):
+    logger.debug("Searching for duplicate checksums from the database.")
     duplicate_checksums = [checksum for checksum in find_duplicate_checksums(session)]
-
+    logger.info(f"Found {len(duplicate_checksums)} duplicate checksums.")
     console = Console()
 
-    duplicates = {}
+    # logger.debug("Loop through each duplicate checksum")
+    # duplicates = {}
     for checksum in duplicate_checksums:
         statement = select(File).filter_by(sha256=checksum)
         duplicates_of_checksum = [dupe_set.name for dupe_set in [row.File for row in session.execute(statement).all()]]
-        duplicates[checksum] = duplicates_of_checksum
+        # duplicates[checksum] = duplicates_of_checksum
 
-    for checksum, files in duplicates.items():
+        number_of_files_found = len(duplicates_of_checksum)
         table = Table(title=f"Duplicates of {checksum}")
+        table.add_column("")
         table.add_column("Parent")
         table.add_column("Filename")
-        table.add_column("Recommend")
-        for file in files:
+        # table.add_column("Recommend")
+        for file_number, file in enumerate(duplicates_of_checksum, start=1):
             this_file = Path(file)
             parent = str(this_file.parent)
             name = str(this_file.name)
-            recommend = recommended_filename(this_file)
-            recommend = str(recommend.name) if recommend else ""
-            table.add_row(parent, name, recommend)
+            # recommend = recommended_filename(this_file)
+            # recommend = str(recommend.name) if recommend else ""
+            table.add_row(str(file_number), parent, name)
         console.print(table)
+
+        while True:
+            keep_number = IntPrompt.ask("Which one to keep?", default=1)
+            if keep_number >= 1 and keep_number <= number_of_files_found:
+                break
+
+        console.print(f"We want to keep file {keep_number} which is {duplicates_of_checksum[keep_number-1]}")
+
+        recommended_filenames = [get_recommended_filename(Path(file)) for file in duplicates_of_checksum]
+        recommended_filenames = {str(file.name) for file in recommended_filenames if file}
+
+        recommended_filename = str(first(recommended_filenames, "N/A"))
+        console.print(f"Recommended filenames: {recommended_filename}")
 
 
 def main():
