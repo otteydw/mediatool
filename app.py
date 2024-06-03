1d841018d2e076e889c55dcd2ae4c689e96edc89
Dan Ottey <dan@bluefrogs.us>
2024-06-02 21:44:10 -0400

    Flask app can now remove duplicates from the filesystem and database. (#16)
    Flask app can now remove duplicates from the filesystem and database.
    Rename function for clarity.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+from ast import literal_eval
 from pathlib import Path
 
 import sqlalchemy as sa
-from flask import Flask, render_template, request, send_from_directory, url_for
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+)
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
 
+from db_utils import consolidate_files_db
 from models import File
-from utils import load_config
+from utils import consolidate_files, load_config
 
 
 class Config:
@@ -25,8 +34,7 @@ def pics(filename):
 
 
 @app.route("/dupes")
-def process_duplicates(page_number=1):
-
+def view_duplicates(page_number=1):
     page = request.args.get("page", page_number, type=int)
     query = (
         sa.select(File.sha256, sa.func.count(File.sha256).label("qty"))
@@ -36,6 +44,13 @@ def process_duplicates(page_number=1):
 
     duplicate_checksums = db.paginate(query, page=page, per_page=1, error_out=False)
 
+    if page > duplicate_checksums.total:
+        page = duplicate_checksums.total
+        return redirect(url_for(request.endpoint, page=page))
+    elif page < 1:
+        page = 1
+        return redirect(url_for(request.endpoint, page=page))
+
     # Since we paginate 1 item per "page", we only care about that first item.
     checksum = list(duplicate_checksums)[0]
 
@@ -44,12 +59,10 @@ def process_duplicates(page_number=1):
         Path(dupe_set.name) for dupe_set in [row.File for row in db.session.execute(statement).all()]
     ]
 
-    prev_url = (
-        url_for("process_duplicates", page=duplicate_checksums.prev_num) if duplicate_checksums.has_prev else None
-    )
-    next_url = (
-        url_for("process_duplicates", page=duplicate_checksums.next_num) if duplicate_checksums.has_next else None
-    )
+    prev_url = url_for("view_duplicates", page=duplicate_checksums.prev_num) if duplicate_checksums.has_prev else None
+    next_url = url_for("view_duplicates", page=duplicate_checksums.next_num) if duplicate_checksums.has_next else None
+
+    all_files = [str(file) for file in duplicates_of_checksum]
 
     return render_template(
         "duplicates.html",
@@ -59,4 +72,18 @@ def process_duplicates(page_number=1):
         checksum=checksum,
         current=duplicate_checksums.page,
         total=duplicate_checksums.total,
+        all_files=all_files,
     )
+
+
+@app.route("/consolidate", methods=["POST"])
+def consolidate():
+    rq = request.form
+    page = int(rq.get("page", "None"))
+    file_to_keep = Path(rq.get("keep_file", "None"))
+    all_files = [Path(file) for file in literal_eval(rq.get("all_files", "None"))]
+
+    consolidate_files(all_files, file_to_keep)
+    consolidate_files_db(db.session, all_files, file_to_keep)
+    # return f"We kept {file_to_keep}<br><br>All files {all_files}"
+    return redirect(url_for("view_duplicates", page=page))
